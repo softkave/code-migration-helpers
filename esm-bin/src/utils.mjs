0749cf3ce1afd7a8d296d216e3029852babb8fa1
Abayomi Akintomide <ywordk@gmail.com>
2024-06-16 16:32:13 -0400

    feat: publish v0.5.4

--- esm-bin/src/utils.mjs
+++ esm-bin/src/utils.mjs
@@ -13,10 +13,14 @@ export function getDirAndBasename(filepath) {
     return undefined;
 }
 export async function traverseAndProcessFilesInFolderpath(folderpath, handleFile, ...args) {
+    const stat = await fsExtra.stat(folderpath);
+    let filepathList = [];
+    const folderpathList = [];
+    const kIterationMax = 20;
     async function internalHandleFile(filepath) {
         try {
-            const modifiedFile = await handleFile(filepath, ...args);
-            if (modifiedFile) {
+            const isModified = await handleFile(filepath, ...args);
+            if (isModified) {
                 console.log(`modified ${filepath}`);
             }
         }
@@ -25,30 +29,46 @@ export async function traverseAndProcessFilesInFolderpath(folderpath, handleFile
             console.error(error);
         }
     }
-    async function internalHandleFolder(folderpath) {
+    async function processNextFolderpath() {
+        const nextFolderpath = folderpathList.shift();
+        if (!nextFolderpath) {
+            return;
+        }
         try {
-            await traverseAndProcessFilesInFolderpath(folderpath, handleFile, ...args);
+            const dirList = await fsExtra.readdir(nextFolderpath, {
+                withFileTypes: true,
+            });
+            dirList.forEach(async (entry) => {
+                const entryPath = path.normalize(path.join(entry.path, entry.name));
+                if (entry.isDirectory()) {
+                    folderpathList.push(entryPath);
+                }
+                else if (entry.isFile()) {
+                    filepathList.push(entryPath);
+                }
+            });
         }
         catch (error) {
-            console.log(`error ${folderpath}`);
+            console.log(`error ${nextFolderpath}`);
             console.error(error);
         }
     }
-    const stat = await fsExtra.stat(folderpath);
+    async function processQueuedFiles() {
+        for (let i = 0; i < filepathList.length; i += kIterationMax) {
+            await Promise.all(filepathList
+                .slice(i, kIterationMax)
+                .map(filepath => internalHandleFile(filepath)));
+        }
+        filepathList = [];
+    }
     if (stat.isDirectory()) {
-        const dirList = await fsExtra.readdir(folderpath, { withFileTypes: true });
-        await Promise.all(dirList.map(async (entry) => {
-            const entryPath = path.normalize(path.join(entry.path, entry.name));
-            if (entry.isDirectory()) {
-                await internalHandleFolder(entryPath);
-            }
-            else if (entry.isFile()) {
-                await internalHandleFile(entryPath);
-            }
-        }));
+        folderpathList.push(folderpath);
     }
     else {
-        await internalHandleFile(folderpath);
+        filepathList.push(folderpath);
+    }
+    while (folderpathList.length || filepathList.length) {
+        await Promise.all([processQueuedFiles(), processNextFolderpath()]);
     }
 }
 export function isRelativeImportText(text) {
